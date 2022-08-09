c8532de2cfe887684e30b137903952210738a6c8
Jew <dima.com655@gmail.com>
2022-08-09 17:22:37 +0300

    Reward Popup

---
+++ Assets/OtherUI/Reward UI/RewardPopup.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using WordConnect;
+
+public class RewardPopup : MonoBehaviour
+{
+    [SerializeField] private LevelCompletePopup levelCompletePopup;
+
+    [SerializeField] private CanvasGroup firstPanel;
+    [SerializeField] private CanvasGroup secondPanel;
+    [SerializeField] private CanvasGroup thirdPanel;
+
+    [SerializeField] private CanvasGroup lastCanvasGroup;
+    private Coroutine currentCorutine;
+
+    [SerializeField] private float hideTime = 0.5f;
+    [SerializeField] private float openTime = 0.5f;
+
+    public void Awake()
+    {
+        OnSetup();
+    }
+
+    public void OnSetup()
+    {
+        firstPanel.alpha = 0;
+        firstPanel.blocksRaycasts = false;
+        
+        secondPanel.alpha = 0;
+        secondPanel.blocksRaycasts = false;
+
+        thirdPanel.alpha = 0;
+        thirdPanel.blocksRaycasts = false;
+    }
+
+    [ContextMenu("Open First")]
+    public void OpenFirstPanel()
+    {
+        OpenWindow(0);
+    }
+
+    private IEnumerator HideWindow(CanvasGroup panelGroup)
+    {
+        panelGroup.DOFade(0, hideTime);
+        yield return new WaitForSeconds(hideTime);
+
+        lastCanvasGroup.blocksRaycasts = false;
+        lastCanvasGroup = null;
+        Debug.Log($"Hide {lastCanvasGroup}");
+    }
+
+    public void OpenWindow(int windowNumber)
+    {
+        if (lastCanvasGroup != null)
+        {
+            StartCoroutine(HideWindow(lastCanvasGroup));
+        }
+
+        switch (windowNumber)
+        {
+            case 0:
+                firstPanel.blocksRaycasts = true;
+                StartCoroutine(OpenWindow(firstPanel));
+                break;
+
+            case 1:
+                secondPanel.blocksRaycasts = true;
+                StartCoroutine(OpenWindow(secondPanel));
+                break;
+
+            case 2:
+                thirdPanel.blocksRaycasts = true;
+                StartCoroutine(OpenWindow(thirdPanel));
+                break;
+
+            case 3:
+                levelCompletePopup.OnBackClicked();
+                break;
+        }
+    }
+
+    private IEnumerator OpenWindow(CanvasGroup panelGroup)
+    {
+        panelGroup.DOFade(1, openTime);
+        yield return new WaitForSeconds(openTime);
+        lastCanvasGroup = panelGroup;
+        Debug.Log($"Open {lastCanvasGroup}");
+    }
+
+}
--- Assets/WordConnect/Scripts/Game/LevelCompletePopup.cs
+++ Assets/WordConnect/Scripts/Game/LevelCompletePopup.cs
@@ -8,20 +8,21 @@ namespace WordConnect
     public class LevelCompletePopup : BBG.Popup
     {
         #region Inspector Variables
+        [SerializeField] private RewardPopup rewardPopup;
 
         [SerializeField] private Image backgroundImage = null;
-        [SerializeField] private Text packNameText = null;
-        [SerializeField] private Text levelText = null;
-        [SerializeField] private Text categoryText = null;
-        [SerializeField] private Text extraWordsText = null;
-        [SerializeField] private RectTransform extraWordsCoinMarker = null;
+        //[SerializeField] private Text packNameText = null;
+        //[SerializeField] private Text levelText = null;
+        //[SerializeField] private Text categoryText = null;
+        //[SerializeField] private Text extraWordsText = null;
+       // [SerializeField] private RectTransform extraWordsCoinMarker = null;
         [SerializeField] private Text nextLevelButtonText = null;
        // [SerializeField] private Text gamePointsText = null;
-        [SerializeField] private Slider categoryProgressSlider = null;
-        [SerializeField] private Image categoryProgressBar = null;
+        //[SerializeField] private Slider categoryProgressSlider = null;
+       // [SerializeField] private Image categoryProgressBar = null;
         
         [Space]
-        [SerializeField] private RectTransform categoryCoinPrizeIcon = null;
+        //[SerializeField] private RectTransform categoryCoinPrizeIcon = null;
 
         #endregion
 
@@ -40,6 +41,9 @@ namespace WordConnect
         {
             base.OnShowing(inData);
 
+            rewardPopup.OnSetup();
+            rewardPopup.OpenWindow(0);
+
             ActiveLevel level = (ActiveLevel)inData[0];
             bool isLevelAlreadyComplete = (bool)inData[1];
             int currentGamePoints = (int)inData[2];
@@ -53,38 +57,38 @@ namespace WordConnect
             bool isLastLevel = (bool)inData[10];
 
             backgroundImage.sprite = level.packInfo.background;
-            packNameText.text = level.packInfo.packName;
-            levelText.text = string.Format("LEVEL {0} COMPLETED", level.levelData.GameLevelNumber);
+            //packNameText.text = level.packInfo.packName;
+           // levelText.text = string.Format("LEVEL {0} COMPLETED", level.levelData.GameLevelNumber);
             //gamePointsText.text = currentGamePoints.ToString();
             nextLevelButtonText.text = isLastLevel ? "HOME" : string.Format("PLAY LEVEL {0}", level.levelData.GameLevelNumber + 1);
 
-            categoryCoinPrizeIcon.gameObject.SetActive(true);
-            extraWordsCoinMarker.gameObject.SetActive(true);
+            //categoryCoinPrizeIcon.gameObject.SetActive(true);
+            //extraWordsCoinMarker.gameObject.SetActive(true);
 
             // If the level is already complete then don't show the category progress or extra words
             if (isLevelAlreadyComplete)
             {
-                categoryText.gameObject.SetActive(false);
-                extraWordsText.gameObject.SetActive(false);
-                categoryProgressSlider.gameObject.SetActive(false);
+                //categoryText.gameObject.SetActive(false);
+                //extraWordsText.gameObject.SetActive(false);
+                //categoryProgressSlider.gameObject.SetActive(false);
             }
             else
             {
-                categoryText.gameObject.SetActive(true);
-                extraWordsText.gameObject.SetActive(extraWordsCoinsAwarded > 0);
-                categoryProgressSlider.gameObject.SetActive(true);
+                //categoryText.gameObject.SetActive(true);
+                //extraWordsText.gameObject.SetActive(extraWordsCoinsAwarded > 0);
+                //categoryProgressSlider.gameObject.SetActive(true);
 
                 int categoryNumberComplete = level.levelData.CategoryLevelNumber;
                 int totalLevelsInCategory = level.categoryInfo.LevelDatas.Count;
 
-                categoryText.text = string.Format("{0} {1} / {2}", level.categoryInfo.displayName, categoryNumberComplete, totalLevelsInCategory);
-                extraWordsText.text = string.Format("+ {0} Extra Words", extraWordsCoinsAwarded);
+                //categoryText.text = string.Format("{0} {1} / {2}", level.categoryInfo.displayName, categoryNumberComplete, totalLevelsInCategory);
+               // extraWordsText.text = string.Format("+ {0} Extra Words", extraWordsCoinsAwarded);
 
                 float categoryProgressFromValue = Mathf.Lerp(0.095f, 1f, (float)(categoryNumberComplete - 1) / (float)totalLevelsInCategory);
                 float categoryProgressToValue = Mathf.Lerp(0.095f, 1f, (float)(categoryNumberComplete) / (float)totalLevelsInCategory);
 
-                categoryProgressSlider.value = categoryProgressFromValue;
-                categoryProgressBar.color = level.packInfo.color;
+                //categoryProgressSlider.value = categoryProgressFromValue;
+                //categoryProgressBar.color = level.packInfo.color;
 
                 animationEnumerator =
                     Animate(
@@ -183,7 +187,7 @@ namespace WordConnect
                 float t = 1f - (float)(timeEnd - Utilities.SystemTimeInMilliseconds) / duration;
                 float value = Mathf.Lerp(categoryProgressFromValue, categoryProgressToValue, t);
 
-                categoryProgressSlider.value = value;
+                //categoryProgressSlider.value = value;
 
                 if (Utilities.SystemTimeInMilliseconds >= timeEnd)
                 {
@@ -205,7 +209,7 @@ namespace WordConnect
 
                 //CoinController.Instance.AnimateCoins(categoryCoinsAmountFrom, categoryCoinsAmountTo, fromPositions);
 
-                categoryCoinPrizeIcon.gameObject.SetActive(false);
+                //categoryCoinPrizeIcon.gameObject.SetActive(false);
             }
 
             yield return new WaitForSeconds(betweenDelay * 2f);
@@ -217,12 +221,12 @@ namespace WordConnect
 
                 for (int i = 0; i < extraWordsCoinsAwarded; i++)
                 {
-                    fromPositions.Add(extraWordsCoinMarker);
+                   // fromPositions.Add(extraWordsCoinMarker);
                 }
 
                 CoinController.Instance.AnimateCoins(extraWordsCoinsAmountFrom, extraWordsCoinsAmountTo, fromPositions);
 
-                extraWordsCoinMarker.gameObject.SetActive(false);
+                //extraWordsCoinMarker.gameObject.SetActive(false);
             }
 
             animationEnumerator = null;
